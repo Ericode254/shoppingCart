61b01975d4e40361e3cb0052b36984332a4789e9
Ericode254 <jilloerick6@gmail.com>
2024-08-13 20:51:45 +0300

    created my navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Navbar as NavbarBs, Container, Nav, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            {" "}
+            Home{" "}
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            {" "}
+            Store{" "}
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            {" "}
+            About{" "}
+          </Nav.Link>
+        </Nav>
+        <Button></Button>
+      </Container>
+    </NavbarBs>
+  );
+};
+
+export default Navbar;
